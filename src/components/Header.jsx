acfe7f945b53dd4ff308279f7cdfcd889a4cb79f
Matt Perroni <matt.perroni@gmail.com>
2026-01-21 16:35:50 -0700

    Fixed header and added gh-pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../styles.css';
 export default function Header() {
   return (
     <div className="header">
-      <img className="logo" src="logo.png" alt="Moviedux Logo" />
+      <img className="logo" src={process.env.PUBLIC_URL + "/logo.png"} alt="Moviedux Logo" />
       <h2 className="app-subtitle">It's time for popcorn.  Find your next movie here.</h2>
     </div>
   );
