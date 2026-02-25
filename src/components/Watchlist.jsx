d52bae14d0123b5326465c523a00c3609eefeb50
Matt Perroni <matt.perroni@gmail.com>
2026-02-25 16:42:52 -0700

    Added movieCards to watchlist

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../styles.css';
+import MovieCard from './MovieCard';
 
 export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   return (
     <div className="title">
       <h1>Your Watchlist</h1>
+      <div className="watchlist">
+        {watchlist.map((movieId) => {
+          const movie = movies.find((m) => m.id === movieId);
+          return (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              isWatchlisted={true}
+              toggleWatchlist={toggleWatchlist}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
