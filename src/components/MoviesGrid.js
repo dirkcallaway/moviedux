29f96b42d9cf72f0954a31fc5f1d95f828b8a638
Matt Perroni <matt.perroni@gmail.com>
2026-02-25 13:02:30 -0700

    Added searchbar and hooked up to filter movies

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,6 +5,9 @@ import '../styles.css';
 export default function MoviesGrid() {
 
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredMovies = movies.filter(movie => movie.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/movies.json`)
@@ -13,14 +16,20 @@ export default function MoviesGrid() {
   }, [])
 
   return (
-    <div className='movies-grid'>
-
-      {
-        movies.map(movie => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))
-      }
-
+    <div>
+      <input
+        type="text"
+        className="search-input"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        placeholder="Search movies..." />
+      <div className='movies-grid'>
+        {
+          filteredMovies.map(movie => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))
+        }
+      </div>
     </div>
   )
 }
