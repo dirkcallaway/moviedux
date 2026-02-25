5545e64ec6b27fb9ccf752439d65a182e576903b
Matt Perroni <matt.perroni@gmail.com>
2026-02-25 16:00:38 -0700

    Added router and home, watchlist routes

---
+++ src/components/Watchlist.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../styles.css";
+
+export default function Watchlist() {
+  return (
+    <div className="title">
+      <h1>Your Watchlist</h1>
+    </div>
+  );
+}
