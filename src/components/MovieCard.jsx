6060a621bf71190e7cf43bc9907e29905446d712
Matt Perroni <matt.perroni@gmail.com>
2026-02-25 12:47:20 -0700

    Added img error handler

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 import '../styles.css';
 
 export default function MovieCard({ movie }) {
+  const handleImgError = (e) => {
+    e.target.src = `${process.env.PUBLIC_URL}/images/placeholder.png`;
+  }
   return (
     <div className='movie-card'>
-      <img src={`${process.env.PUBLIC_URL}/images/${movie.image}`} alt={movie.title} />
+      <img src={`${process.env.PUBLIC_URL}/images/${movie.image}`} alt={movie.title} onError={handleImgError} />
       <div className='movie-card-info'>
         <h3 className='movie-card-title'>{movie.title}</h3>
         <p className='movie-card-genre'>{movie.genre}</p>
